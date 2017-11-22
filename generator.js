788b124633454079b592e2911bcabc0d04676a4b
Dima Goltsman <dimitrig@wix.com>
2017-11-22 21:28:15 +0200

    refactor

--- generator.js
+++ generator.js
@@ -2,10 +2,10 @@
 var HEX = ["240d", "0d24"];
 var RF433 = "b2";
 var RF315 = "d7";
-var END = "0c00016f00000000";
+var FOOTER = "0c00016f00000000";
 
 
-var BYTES = 24;
+var BITS = 24;
 var LONG_REPEAT = "9";
 
 function typePrefixOf(type){
@@ -18,31 +18,23 @@ function typePrefixOf(type){
   }
 }
 
-function randomOnes(){
-  return ONES[Math.floor(Math.random()*ONES.length)];
-}
 
-function randomZeros(){
-  return ZEROS[Math.floor(Math.random()*ZEROS.length)];
-}
-
-
-function randomHexNumber(){
+function randomPulse(){
   return HEX[Math.floor(Math.random()*HEX.length)];
 }
 
 
 function generate(type){
   var code = "";
-  for (i = 0; i < BYTES; i++) {
-    var rand = randomHexNumber()
+  for (i = 0; i < BITS; i++) {
+    var rand = randomPulse();
     code = code + rand;
   }
 
   var typePrefix = typePrefixOf(type);
 
-  var res           = typePrefix + "0c"      + "3400" + code + END;
-  var resWithRepeat = typePrefix + LONG_REPEAT + "3400" + code + END;
+  var res           = typePrefix + "0c"      + "3400" + code + FOOTER;
+  var resWithRepeat = typePrefix + LONG_REPEAT + "3400" + code + FOOTER;
 
   return {
           regular: hexToBase64(res),
