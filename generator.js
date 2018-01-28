0ca721f5e0746f1d1a299ec2acb30d134037b0b5
Dima Goltsman <dimitrig@wix.com>
2018-01-28 16:24:10 +0200

    adding livolo generator

--- generator.js
+++ generator.js
@@ -10,6 +10,23 @@ var LONG_REPEAT = "5c";
 var BYTES = 24;
 var DATA_LENGTH = "3400";
 
+
+
+String.prototype.rightJustify = function( length, char ) {
+    var fill = [];
+    while ( fill.length + this.length < length ) {
+        fill[fill.length] = char;
+    }
+    return this + fill.join('');
+}
+String.prototype.leftJustify = function( length, char ) {
+    var fill = [];
+    while ( fill.length + this.length < length ) {
+        fill[fill.length] = char;
+    }
+    return fill.join('') + this;
+}
+
 function typePrefixOf(type){
   if(type === "RF433"){
     return RF433;
@@ -66,6 +83,60 @@ function getNewCode(b64, repeats){
   return hexToBase64(res);
 
 }
+
+
+
+function generateLivolo(remoteId, btn){
+    header = "b280260013";
+    id_bin = (+remoteId).toString(2);
+    id_bin = id_bin.leftJustify(16,0);
+    btn_bin = (+btn).toString(2);
+    btn_bin = btn_bin.leftJustify(7,0);
+
+    id_btn_bin = id_bin.concat(btn_bin);
+
+    id_btn_bin = id_btn_bin.replace(/0/g, "0606");
+    id_btn_bin = id_btn_bin.replace(/1/g, "0c");
+
+
+    hex_out = header + id_btn_bin;
+
+    pad_len = 32 - (hex_out.length - 24) % 32;
+
+
+    hex_out = hex_out + ('').leftJustify(pad_len,0);
+    //alert(hex_out);
+
+    return hexToBase64(hex_out);
+
+
+    /*
+
+    https://www.tyjtyj.com/livolo.php
+    Buttons:
+    <option value='90'  >on only(scn1)</option>
+	<option value='40'  >on/off (toggle/btn10)</option>
+	<option value='106' >off only</option>
+	<option value='0'   >btn1</option>
+	<option value='96'	>btn2</option>
+	<option value='120'	>btn3</option>
+	<option value='24'  >btn4</option>
+	<option value='108' >btn5</option>
+	<option value='80'  >btn6</option>
+	<option value='48'  >btn7</option>
+	<option value='12'  >btn8</option>
+	<option value='72'  >btn9</option>
+	<option value='40'  >btn10</option>
+	<option value='90'  >scn1</option>
+	<option value='114' >scn2</option>
+	<option value='10'  >scn3</option>
+	<option value='18'  >scn4</option>
+     */
+}
+
+
+
+
 
 
 
